79aeff76298c635d5939b78e48c82a74df6175c6
Mek305 <Mek305@gmail.com>
2019-08-21 20:05:41 -0400

    My Unfinised EventListener

---
+++ script.js
@@ -0,0 +1,15 @@
+var boxs= document.getElementById('box')
+
+boxs.addEventListener('mouseover',changeColor)
+boxs.addEventListener('mouseleave',changeColor)
+
+
+function changeColor() {
+	if(box.style.backgroundColor == 'white'){
+		box.style.backgroundColor == 'black'
+	}else{
+		
+		box.style.backgroundColor == 'white'
+	}
+}
+
